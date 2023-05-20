fa415742b3c034d4bc7aa1e505349c51dc36d677
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2023-05-19 20:00:22 -0500

    updated code to use c strings

--- src/main.cpp
+++ src/main.cpp
@@ -99,7 +99,7 @@ void sample_humidity()
 }
 
 // Variable to store the HTTP request
-char header[256];
+char header[2048];
 // Current time
 unsigned long currentTime = millis();
 // Previous time
@@ -166,9 +166,11 @@ void handleNetwork()
   if (client)
   {                                // If a new client connects,
     Serial.println("New Client."); // print a message out in the serial port
-    char currentLine[256] = "";    // make a C string to hold incoming data from the client
+    char currentLine[1024] = "";    // make a C string to hold incoming data from the client
     currentTime = millis();
     previousTime = currentTime;
+    size_t headerLen = 0;
+
     while (client.connected() && currentTime - previousTime <= timeoutTime)
     { // loop while the client's connected
       currentTime = millis();
@@ -176,7 +178,17 @@ void handleNetwork()
       {                         // if there's bytes to read from the client,
         char c = client.read(); // read a byte, then
         Serial.write(c);        // print it out the serial monitor
-        strncat(header, &c, 1);
+
+        if (headerLen < sizeof(header) - 2)
+        {
+          header[headerLen++] = c;
+          header[headerLen] = '\0';
+        }else{
+          Serial.println("header buffer exceeded");
+          client.write("header buffer limit exceeded");
+          break;
+        }
+
         if (c == '\n')
         { // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
@@ -194,7 +206,10 @@ void handleNetwork()
         }
         else if (c != '\r')
         {                   // if you got anything else but a carriage return character,
-          strncat(currentLine, &c, 1); // add it to the end of the currentLine
+          if (strlen(currentLine) < sizeof(currentLine) - 2)
+          {
+            strncat(currentLine, &c, 1); // add it to the end of the currentLine
+          }
         }
       }
     }
@@ -209,6 +224,7 @@ void handleNetwork()
 
 void loop()
 {
+  MDNS.update();
   handleNetwork();
   alarmActivated = humidity > alarmHumidity;
   sample_humidity();
